9b69fbce58cc43463635d949465880927d0e421f
nikita@aveldent.ru <Ni5020830>
2023-04-05 12:33:59 +0300

    Добавил модели для создания товара и миграции

---
+++ database/migrations/2023_04_05_085224_create_product_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('category_id');
+            $table->string('articul');
+            $table->string('brand');
+            $table->integer('quantity');
+            $table->integer('status');
+            $table->decimal('price');
+            $table->text('description');
+            $table->integer('image_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
